a47410af26dd700058140c6a2f50120190bd2273
nicolas <TuCorreo>
2025-01-01 12:57:27 -0300

    visualizar productos

--- js/productos.js
+++ js/productos.js
@@ -60,8 +60,24 @@ const productosOferta = [
   }
 ];
 
-const contenedorProductos = document.querySelector("#productos");
+
 const contenedorOfertas = document.querySelector(".productos-oferta");
+const contenedorProductos = document.querySelector('.productos');
+
+productos.forEach(producto => {
+const tarjeta = document.createElement('div');
+tarjeta.classList.add('producto-caja');
+
+tarjeta.innerHTML = `
+    <img class="producto-imagen" src="${producto.imagen}" alt="${producto.nombre}">
+    <h3 class="producto-nombre">${producto.nombre}</h3>
+    <p class="producto-descripcion">${producto.descripcion}</p>
+    <p class="producto-precio">$${producto.precio}</p>
+    <button class="add-carrito">Agregar al carrito</button>
+`;
+
+contenedorProductos.appendChild(tarjeta);
+});
 
 
 function renderizarProductos(productos, contenedor) {
@@ -88,3 +104,7 @@ if (contenedorProductos) {
 if (contenedorOfertas) {
     renderizarProductos(productosOferta, contenedorOfertas);
 }
+
+
+
+
