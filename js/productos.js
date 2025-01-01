e32f054f792f2dd543c2fd2abc514cb540871804
nicolas <TuCorreo>
2025-01-01 12:45:58 -0300

    visualizar productos

--- js/productos.js
+++ js/productos.js
@@ -4,28 +4,28 @@ const productos = [
       nombre: "Buzo Hunter x Hunter",
       precio: 25000,
       descripcion: "Buzo HUNTER X HUNTER",
-      imagen: "../imagenes/productos/buzos/buzo-HxH.jpg"
+      imagen: "/uwu/imagenes/productos/buzos/buzo-HxH.jpg"
   },
   {
       id: 2,
       nombre: "Buzo My Hero Academia",
       precio: 25000,
       descripcion: "Buzo de My Hero Academia",
-      imagen: "../imagenes/productos/buzos/buzo-MHA.jpg"
+      imagen: "/uwu/imagenes/productos/buzos/buzo-MHA.jpg"
   },
   {
       id: 3,
       nombre: "Buzo Nana",
       precio: 25000,
       descripcion: "Buzo de Nana",
-      imagen: "../imagenes/productos/buzos/buzo-nana.jpg"
+      imagen: "/uwu/imagenes/productos/buzos/buzo-nana.jpg"
   },
   {
       id: 4,
       nombre: "Buzo Attack on Titan",
       precio: 20000,
       descripcion: "Buzo de algodón color verde",
-      imagen: "../imagenes/productos/buzos/buzo-aot.jpg"
+      imagen: "/uwu/imagenes/productos/buzos/buzo-aot.jpg"
   }
 ];
 
@@ -35,28 +35,28 @@ const productosOferta = [
       nombre: "Bolso Haikyu",
       precio: 25000,
       descripcion: "Bolso Haikyu",
-      imagen: "../imagenes/productos/ofertas/bolso-haikyu.jpg"
+      imagen: "/uwu/imagenes/productos/ofertas/bolso-haikyu.jpg"
   },
   {
       id: 2,
       nombre: "Peluche Gojo",
       precio: 25000,
       descripcion: "Peluche de Satoru Gojo",
-      imagen: "../imagenes/productos/ofertas/peluche-Jujutsu Kaisen-Satoru Gojo.jpg"
+      imagen: "/uwu/imagenes/productos/ofertas/peluche-Jujutsu Kaisen-Satoru Gojo.jpg"
   },
   {
       id: 3,
       nombre: "Haori Naruto",
       precio: 25000,
       descripcion: "Haori de Naruto",
-      imagen: "../imagenes/productos/ofertas/ropa-naruto.jpg"
+      imagen: "/uwu/imagenes/productos/ofertas/ropa-naruto.jpg"
   },
   {
       id: 4,
       nombre: "Taza Demon Slayer",
       precio: 25000,
       descripcion: "Taza de Demon Slayer",
-      imagen: "../imagenes/productos/ofertas/taza-Demon-Slayer.jpg"
+      imagen: "/uwu/imagenes/productos/ofertas/taza-Demon-Slayer.jpg"
   }
 ];
 
